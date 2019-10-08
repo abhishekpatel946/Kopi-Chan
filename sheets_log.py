6c2d17aef1a667e64229a3a810082b18c05691f8
Jakkarin Sae-Tiew <jakkarin.sae-tiew@outlook.com>
2019-10-08 10:18:44 +0800

    Minor fix sheets_log.py

--- sheets_log.py
+++ sheets_log.py
@@ -3,8 +3,8 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name(
-    'credentials.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+
 client = gspread.authorize(creds)
 
 
