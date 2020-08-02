9f1d76eb8f3fb5584801f408911d5b78e6ea06be
Jakkarin Sae-Tiew <jakkarin.sae-tiew@outlook.com>
2020-08-02 23:59:27 +0800

    Added credentials for deployment

---
+++ credentials/firebase_config.py
@@ -0,0 +1,11 @@
+config = {
+    "apiKey": "AIzaSyB3uWHGYINp6J6P2f-R-vYpM89kQorb_Uo",
+    "authDomain": "uscaffeinatedbot.firebaseapp.com",
+    "databaseURL": "https://uscaffeinatedbot.firebaseio.com",
+    "projectId": "uscaffeinatedbot",
+    "storageBucket": "uscaffeinatedbot.appspot.com",
+    "messagingSenderId": "900893280992",
+    "appId": "1:900893280992:web:926c15a22f1bb5462e1e85",
+    "measurementId": "G-FY7W46L3S6",
+    "serviceAccount": "credentials/firebase_service_account_credentials.json"
+}
---
+++ credentials/telegram_token.py
@@ -0,0 +1,2 @@
+# Telegram bot token
+TOKEN = "908143577:AAEjKlF05FauSivmwYeQ1Hv1HHZRlLaNHsw"
